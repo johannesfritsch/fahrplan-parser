f24f473d7c0f1e188a0bebee4d9aff13292e1078
Johannes Fritsch <fritsch@zoom7.de>
2015-12-10 12:44:01 +0100

    Find current event in room

--- c3data.js
+++ c3data.js
@@ -20,6 +20,13 @@ var upcomingEventsOfTheDay = _.take(_.filter(eventsOfTheDay, function(e){
 	return moment(e.date).isAfter(now);
 }), 2);
 
+var upcomingEventInRoom1 = _.chain(currentDay.rooms['Saal 1']).filter(function(evt){ return moment(evt.date).isBefore(now)}).sortBy(function(a, b){  
+  if(moment(a.date).isBefore(moment(b.date))) 
+    return -1;
+  else
+    return 1;
+}).last().value();
+
 console.log('NOW: ' + now);
 console.log('');
 console.log('EVENTS OF THE DAY:');
@@ -27,4 +34,7 @@ console.log(_.map(eventsOfTheDay, function(e){ return '- ' + e.title + ' ('+ mom
 console.log('');
 console.log('TWO NEXT UPCOMING EVENTS OF THE DAY:');
 console.log(_.map(upcomingEventsOfTheDay, function(e){ return '- ' + e.title + ' ('+ moment(e.date).format('H:mm') +')' }).join("\n"));
+console.log('');
+console.log('CURRENT EVENT IN ROOM 1:');
+console.log('- ' + upcomingEventInRoom1.title + ' ('+ moment(upcomingEventInRoom1.date).format('H:mm') +')');
 console.log('');
